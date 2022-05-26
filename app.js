9847c161bb0982aeaafab1486088ab603e8e714e
isiis <Issabobobaldepro@gmail.com.com>
2022-05-26 04:31:00 +0200

    site v1.0

--- app.js
+++
@@ -1,9 +0,0 @@
-let toggle = document.querySelector('.toggle')
-let body = document.querySelector('body')
-let content = document.querySelector('.content')
-
-toggle.addEventListener('click', function(){
-    body.classList.toggle('open');
-    content.classList.toggle('open');
-}
-)
